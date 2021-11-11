84d4572ca54e6bef9d1106c74a8d73f14691ddda
Yurii Tykhun <yura_tyhun95@ukr.net>
2021-11-11 16:16:58 +0200

    add solution with regexp

--- isValueExistsInTree.js
+++ isValueExistsInTree.js
@@ -15,6 +15,12 @@ const isValueExistsInTree = (arr, value) => helper(arr).includes(value);
 //#2
 const isValueExistsInTree = (arr, value) => arr.flat(Infinity).includes(value);
 
+//#3
+const isValueExistsInTree = (arr, value) => {
+   let arrIntoString = JSON.stringify(arr).replace(/\[|\]|,null/g,'');
+   return JSON.parse(`[${arrIntoString}]`).includes(value);
+}
+
 console.log(isValueExistsInTree(arr1, 5));
 console.log(isValueExistsInTree(arr1, 9));
 console.log(isValueExistsInTree(arr1, 7));
