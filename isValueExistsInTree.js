bd4ebd3c64b5401defcb309635f8480af516a389
Yurii Tykhun <yura_tyhun95@ukr.net>
2021-11-11 15:30:56 +0200

    added solution for quiz task

---
+++ isValueExistsInTree.js
@@ -0,0 +1,21 @@
+const arr1 = [3, [8, [5, null, null], null], [7, null, null]];
+
+//#1
+function helper(arr, res = []) {
+    for(let el of arr) {
+        if(Array.isArray(el)) helper(el, res);
+        else if(el === null) continue;
+        else res.push(el);
+    }
+    return res;
+}
+
+const isValueExistsInTree = (arr, value) => helper(arr).includes(value);
+
+//#2
+const isValueExistsInTree = (arr, value) => arr.flat(Infinity).includes(value);
+
+console.log(isValueExistsInTree(arr1, 5));
+console.log(isValueExistsInTree(arr1, 9));
+console.log(isValueExistsInTree(arr1, 7));
+console.log(isValueExistsInTree(arr1, 51));
